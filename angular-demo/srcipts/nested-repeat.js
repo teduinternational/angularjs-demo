1fb9bd39377d22575db041d1466a02a2c6cb7402
ToanBN <tedu.international@gmail.com>
2016-06-20 21:51:11 +0700

    ng repeat directive

---
+++ angular-demo/srcipts/nested-repeat.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    var app = angular.module('app', []);
+    app.controller('nestedRepeatController', ['$scope', function ($scope) {
+        var countries = [
+                      {
+                          name: "UK",
+                          cities: [
+                              { name: "London" },
+                              { name: "Birmingham" },
+                              { name: "Manchester" }
+                          ]
+                      },
+                      {
+                          name: "USA",
+                          cities: [
+                              { name: "Los Angeles" },
+                              { name: "Chicago" },
+                              { name: "Houston" }
+                          ]
+                      },
+                      {
+                          name: "India",
+                          cities: [
+                              { name: "Hyderabad" },
+                              { name: "Chennai" },
+                              { name: "Mumbai" }
+                          ]
+                      }
+        ];
+        $scope.countries = countries;
+    }]);
+})();
---
+++ angular-demo/srcipts/repeat.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+    var app =  angular.module('app',[]);
+    app.controller('repeatController', repeatController);
+
+    repeatController.$inject = ['$scope'];
+
+    function repeatController($scope) {
+        var employees = [
+                   { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000 },
+                   { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000 },
+                   { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000 },
+                   { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000 },
+                   { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
+        ];
+        $scope.employees = employees;
+    }
+})();
