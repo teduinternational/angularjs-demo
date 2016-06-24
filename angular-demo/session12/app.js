34c6516c91221f1c06ffd02fae8f0fb9d32e5f2f
ToanBN <tedu.international@gmail.com>
2016-06-24 17:30:29 +0700

    ng-hide and ng-show

---
+++ angular-demo/session12/app.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    var app = angular.module('app', []);
+
+    app.controller('showHideCtrl', showHideCtrl);
+
+    showHideCtrl.$inject = ['$scope'];
+
+    function showHideCtrl($scope) {
+        var employees = [
+            { name: 'Hải', birthDate: new Date('12/3/1989'), salary: 15000000, gender: 'Male', status: true },
+            { name: 'Hoàng', birthDate: new Date('6/15/1990'), salary: 12000000, gender: 'Male', status: true },
+            { name: 'Long', birthDate: new Date('1/14/1992'), salary: 17000000, gender: 'Male', status: false },
+            { name: 'Trung', birthDate: new Date('12/3/1993'), salary: 12000000, gender: 'Male', status: true },
+            { name: 'Hương', birthDate: new Date('12/3/1995'), salary: 11000000, gender: 'Female', status: false },
+            { name: 'Thủy', birthDate: new Date('12/3/1988'), salary: 10000000, gender: 'Female', status: true }
+        ];
+        $scope.employees = employees;
+        $scope.sortColumn = 'name';
+        $scope.reverse = false;
+        $scope.searchText = '';
+        $scope.hideSalary = false;
+        $scope.sortData = function (column) {
+            if ($scope.sortColumn == column)
+                $scope.reverse = !$scope.reverse;
+            else
+                $scope.reverse = false;
+            $scope.sortColumn = column;
+        }
+        $scope.getSortClass = function (column) {
+            if ($scope.sortColumn == column) {
+                return $scope.reverse ? 'arrow-up' : 'arrow-down';
+            }
+            return '';
+        }
+    }
+
+    app.filter('status', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})()
